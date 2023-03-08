e32b0378fc2e50a8aabc1761f0aaa04866da580a
Eduardo Rabelo <oieduardorabelo@gmail.com>
2023-03-08 21:08:48 +1300

    docs

--- cicd/deploy.ts
+++ cicd/deploy.ts
@@ -1,3 +1,12 @@
+/**
+ * To be able to run `ts-node` from the `scripts` block in `package.json`
+ *     For example:
+ *         "scripts": {
+ *             "cicd": "ts-node cicd/deploy.ts"
+ *         }
+ * We need to change `tsconfig.json` file and add the `ts-node` section to transform modules to commonjs
+ * https://typestrong.org/ts-node/docs/configuration/
+ */
 import { Chance } from 'chance';
 
 const chance = new Chance();
